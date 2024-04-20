acd756c3f9e8359a92293fac97dcaf5f9b5d5c2a
elmanitas <mr.daniel_mercado@yahoo.com>
2024-04-20 12:46:40 -0400

    My second attempt at assignment9, after review.

--- asgmt9-solution/src/menuapp/menudata.service.js
+++ asgmt9-solution/src/menuapp/menudata.service.js
@@ -15,16 +15,11 @@
                 url: 'https://coursera-jhu-default-rtdb.firebaseio.com/categories.json'
             }).then(function (result) {
                 var categories = [];
-                
-                console.log(result);
 
                 // iterate over each category
                 for(var index in result.data){
                     categories.push(result.data[index]);
                 }
-
-                console.log('In getAllCategories');
-                console.log(categories);
                 // return processed items
                 return categories;
             });
@@ -32,7 +27,6 @@
         service.getItemsForCategory = function(categoryShortName) {
             return $http({
                 method: "GET",
-                //url: 'https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/{categoryShortName}.json'
                 url: 'https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/' + categoryShortName + '.json'
             }).then(function (result) {
                 var items = []; 
@@ -41,7 +35,6 @@
                     items.push(result.data['menu_items'][index]);
                 }
                 // return processed items
-                console.log(items);
                 return items;
             });
         };    
